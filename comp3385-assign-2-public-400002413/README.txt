d67719b241bfa9273cc376d2c69359f69eb4bce4
AlgoMeta <47063578+AlgoMeta@users.noreply.github.com>
2020-11-13 14:56:00 -0400

    third commit

---
+++ comp3385-assign-2-public-400002413/README.txt
@@ -0,0 +1,3 @@
+1) The index.php file acts as a router for all the other pages so just use the links on the pages.
+2) Inorder to run the SessionManagerTest use the --stderr option. For example, "phpunit --testdox --stderr SessionManagerTest.php"
+3) There is a PHPUnit file inside my test directory because I have a Linux system and it was best practice.
---
+++ comp3385-assign-2-public-400002413/index.php
@@ -0,0 +1,18 @@
+<?php
+    require "../../comp3385-assign-2-framework-400002413/autoloader.php";
+
+    $frontController = new FrontController();
+
+    $frontController->getRequestHandler()->route("/", "IndexCommand");
+    $frontController->getRequestHandler()->route("/index.php", "IndexCommand");
+    $frontController->getRequestHandler()->route("/index.php?controller=SignUp", "SignUpCommand");
+    $frontController->getRequestHandler()->route("/index.php?controller=Login", "LoginCommand");
+    $frontController->getRequestHandler()->route("/index.php?controller=Profile", "ProfileCommand");
+    $frontController->getRequestHandler()->route("/index.php?controller=Courses", "CoursesCommand");
+    $frontController->getRequestHandler()->route("/index.php?controller=Streams", "StreamsCommand");
+    $frontController->getRequestHandler()->route("/index.php?controller=AboutUs", "AboutCommand");
+    $frontController->getRequestHandler()->route("/index.php?controller=Logout", "LogoutCommand");
+
+    $action = basename($_SERVER['REQUEST_URI']);
+    $frontController->getRequestHandler()->dispatch($action);
+?>
